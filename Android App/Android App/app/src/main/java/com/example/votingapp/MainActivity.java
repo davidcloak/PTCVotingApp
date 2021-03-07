05fdd816f55336c92c32c9a086d13f328376b8b2
zhangfengqi937 <zhangfengqi937@gmail.com>
2021-03-07 17:39:59 -0500

    Modify some pages
    Modify register status, changed information page based on user information, add mayor page design. Add hashpassword classes.

--- Android App/Android App/app/src/main/java/com/example/votingapp/MainActivity.java
+++ Android App/Android App/app/src/main/java/com/example/votingapp/MainActivity.java
@@ -1,20 +1,18 @@
 package com.example.votingapp;
 
-import android.content.Intent;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.hardware.biometrics.BiometricPrompt;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CancellationSignal;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.biometric.BiometricManager;
-import androidx.biometric.BiometricPrompt;
-import androidx.core.content.ContextCompat;
-
 import java.util.concurrent.Executor;
-
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,80 +20,85 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        TextView txtMsg = findViewById(R.id.textMessage);
-        Button loginAuth = findViewById(R.id.touchButton);
+    public void test(View view){
+        Toast.makeText(MainActivity.this, "method called :)", Toast.LENGTH_LONG).show();
+    }
 
-        BiometricManager bm = BiometricManager.from(this);
-        //Check user if has bio sensor ready to auth
-        switch (bm.canAuthenticate()){
-            case BiometricManager.BIOMETRIC_SUCCESS:
-                txtMsg.setText("Bio sensor detected");
-                break;
-
-            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
-                txtMsg.setText("Bio sensor not detected");
-                loginAuth.setVisibility(View.GONE);
-                break;
-
-            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
-                txtMsg.setText("Bio sensor unavailable");
-                loginAuth.setVisibility(View.GONE);
-                break;
-
-            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
-                txtMsg.setText("You don't have any fingerprint, go to create one!");
-                loginAuth.setVisibility(View.GONE);
-                break;
-        }
-
-        Executor executor = ContextCompat.getMainExecutor(this);
-        //Check if auth success or not
-        BiometricPrompt bp = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
-            //Auth error
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    public void BioID(View view) {
+        Executor executor = Executors.newSingleThreadExecutor();
+        MainActivity activity = this;
+        BiometricPrompt biometricPrompt = new BiometricPrompt.Builder(this).setTitle("Fingerprint Authentication").setSubtitle("Complete to login.").setDescription("You must confirm your identity with your thumb print.").setNegativeButton("Cancel", executor, new DialogInterface.OnClickListener() {
             @Override
-            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
-                super.onAuthenticationError(errorCode, errString);
+            public void onClick(DialogInterface dialogInterface, int i) {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Authentication Cancelled ", Toast.LENGTH_LONG).show();
+//                            if(captchaBox.isChecked()){
+//                                captchaBox.setChecked(false);
+//                            }
+                    }
+                });
+            }
+        }).build();
+
+
+        biometricPrompt.authenticate(new CancellationSignal(), executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, CharSequence errString) {
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this,"Authentication Cancelled ",Toast.LENGTH_LONG).show();
+//                            if(captchaBox.isChecked()){
+//                                captchaBox.setChecked(false);
+//                            }
+                        }
+                    });
             }
 
-            //Auth success and go to login page
             @Override
-            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
-                super.onAuthenticationSucceeded(result);
-                Toast.makeText(getApplicationContext(), "Auth Success!", Toast.LENGTH_SHORT).show();
-                openLoginPage();
+            public void onAuthenticationHelp(int helpCode, CharSequence helpString) {
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this,"Authentication Cancelled ",Toast.LENGTH_LONG).show();
+//                            if(captchaBox.isChecked()){
+//                                captchaBox.setChecked(false);
+//                            }
+                        }
+                    });
             }
-            //Auth fail
+
+            @Override
+            public void onAuthenticationSucceeded(BiometricPrompt.AuthenticationResult result) {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Authentication Succeeded", Toast.LENGTH_LONG).show();
+//                            if(!captchaBox.isChecked()){
+//                                captchaBox.setChecked(true);
+//                            }
+                    }
+                });
+            }
+
             @Override
             public void onAuthenticationFailed() {
-                super.onAuthenticationFailed();
-                Toast.makeText(getApplicationContext(), "Auth Failed, try again!", Toast.LENGTH_SHORT).show();
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this,"Authentication Cancelled ",Toast.LENGTH_LONG).show();
+//                            if(captchaBox.isChecked()){
+//                                captchaBox.setChecked(false);
+//                            }
+                        }
+                    });
             }
         });
-
-        //Bio sensor check screen
-        BiometricPrompt.PromptInfo pi = new BiometricPrompt.PromptInfo.Builder()
-                .setTitle("Login")
-                .setDescription("Use your fingerprint to login")
-                .setNegativeButtonText("Cancel")
-                .build();
-
-        //touch button onClick event
-        loginAuth.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                bp.authenticate(pi);
-            }
-        });
-
-
     }
-
-    //open login page
-    public void openLoginPage(){
-        Intent intent = new Intent(this, login_page.class);
-        startActivity(intent);
-    }
-
 
 }
