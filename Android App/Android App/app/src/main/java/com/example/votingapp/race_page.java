dd7a41111bea3e7c2eae15fc99e01123eae9be9d
zhangfengqi937 <zhangfengqi937@gmail.com>
2021-03-12 13:12:57 -0500

    Restore to normal files
    Restore

--- Android App/Android App/app/src/main/java/com/example/votingapp/race_page.java
+++ Android App/Android App/app/src/main/java/com/example/votingapp/race_page.java
@@ -2,13 +2,59 @@ package com.example.votingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class race_page extends AppCompatActivity {
+
+    private Button presidentButton;
+    private Button mayorButton;
+    private Button senatorButton;
+    private Button dogCatcherButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_race_page);
+
+        presidentButton = findViewById(R.id.presidentButton);
+        mayorButton = findViewById(R.id.mayorButton);
+        senatorButton = findViewById(R.id.senatorButton);
+        dogCatcherButton = findViewById(R.id.dogCatcherButton);
+
+        presidentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPresidentPage();
+            }
+        });
+
+        mayorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMayorPage();
+            }
+        });
+
+
     }
+
+    public void openPresidentPage(){
+        Intent intent = new Intent(this, president_page.class);
+        startActivity(intent);
+    }
+
+    public void openMayorPage(){
+        Intent intent = new Intent(this, mayor_page.class);
+        startActivity(intent);
+    }
+
+    //Open senator page
+
+    //Open dogCatcher page
+
+
 }
